3edc1cb7ae4ad23187cce67947868994690ab62b
alumno <patriciosalvador@live.com.ar>
2017-11-14 22:47:20 -0300

    lala

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
-  Rails.application.routes.draw do
-      devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-    end
-    
+
+
+Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+    root 'question#index'
+  end
+
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
