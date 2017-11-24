bcaec3dec130ea3f2b0cb535d67cd2cd66bc1e51
Lemeze <eze_gake@hotmail.com>
2017-11-24 19:56:45 -0300

    tormenta

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20171123223917) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answercomments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "answer_id"
+    t.bigint "user_id"
+    t.bigint "answer_id"
     t.text "texto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,8 +26,8 @@ ActiveRecord::Schema.define(version: 20171123223917) do
   end
 
   create_table "answers", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "question_id"
+    t.bigint "user_id"
+    t.bigint "question_id"
     t.text "texto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 20171123223917) do
     t.text "texto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
@@ -84,4 +87,9 @@ ActiveRecord::Schema.define(version: 20171123223917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answercomments", "answers"
+  add_foreign_key "answercomments", "users"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
 end
