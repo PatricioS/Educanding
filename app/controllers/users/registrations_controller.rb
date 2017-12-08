fde1aff2f394e29e1338d676edbb28cbf98acaa2
Lemeze <eze_gake@hotmail.com>
2017-12-08 13:50:44 -0300

    Edita facultad

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_sign_up_params, only:[:create]
-   before_action :configure_sign_edit_params, only:[:edit]
 
   # GET /resource/sign_up
    #  def new
@@ -20,9 +19,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
    end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+   def update
+    super
+   end
 
   # DELETE /resource
     # def destroy
@@ -51,9 +50,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
       devise_parameter_sanitizer.permit(:sign_up, keys: [:facultad_id])
    end
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :apellido, :facultad_id])
+   def account_update_params
+      params.require(:user).permit(:nombre, :apellido, :facultad_id)
    end
+
+   protected
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
+    private :account_update_params
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
 
   resources :tags
-  resources :tabs
   resources :etiqueta
   resources :questions do
       post 'sumar_puntaje'
